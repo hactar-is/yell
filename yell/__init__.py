6824d4ef45273a87c730a5333e12a548784f353e
Andy Beaumont <andy@andybeaumont.com>
2017-09-04 14:13:41 +0100

    Lots of PEP8 tidying and updated the metaclass to work with Python 3

--- yell/__init__.py
+++ yell/__init__.py
@@ -1,43 +1,78 @@
+# -*- coding: utf-8 -*-
+
+"""
+    yell
+    ~~~~~~~~~~~~~
+    Pluggable notifications.
+"""
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from future import standard_library
+standard_library.install_aliases()
+
+from builtins import *
+from builtins import object
+from future.utils import with_metaclass
+
 __version__ = "0.3.2"
 
-from .registry import registry
+
+import hues
+
+
+class Reg(object):
+    """ Registry mapping notification names to backends """
+
+    notifications = {}
+
+    def __new__(cls):
+        obj = super(Reg, cls).__new__(cls)
+        obj.notifications = {}
+        return obj
+
+
+registry = Reg()
 
 
 class MetaNotification(type):
-    """ 
+    """
     Metaclass that stores all notifications in the registry.
     """
-    def __new__(cls, name, bases, attrs):
-        Notification = super(MetaNotification, cls).__new__(cls, name, bases, attrs)
+    def __init__(self, name, bases, attrs):
+        super(MetaNotification, self).__init__(name, bases, attrs)
+        reg = registry.notifications
+        hues.info("META NOTIFICATION")
+        hues.info(self)
+        if self.name is not None:
+            reg[self.name] = reg.get(self.name, []) + [self]
 
-        if Notification.name is not None:
-            registry.notifications[Notification.name] = registry.notifications.get(Notification.name, []) + [Notification]
+            sendfn = lambda *args, **kwargs: notify(self.name, backends=[self], *args, **kwargs)
+            setattr(self, 'send', staticmethod(sendfn))
 
-            sendfn = lambda *args, **kwargs: notify(Notification.name, backends = [Notification], *args, **kwargs)
-            setattr(Notification, 'send', staticmethod(sendfn))
-
-        return Notification
+        return
 
 
-class Notification(object):
+class Notification(with_metaclass(MetaNotification, object)):
     """
     Base class for any kind of notifications. Inherit from this class to create
-    your own notification types and backends. 
-    
+    your own notification types and backends.
+
     Subclasses need to implement :meth:`notify`.
     """
-    __metaclass__ = MetaNotification
 
     name = None
     """
     A name for this notification.
     """
-    
+
     data = {}
     """
     Allow arbitrary data to be stored in the base notification.
     """
-
 
     def notify(self, *args, **kwargs):
         """
@@ -45,29 +80,28 @@ class Notification(object):
         """
         raise NotImplementedError
 
+
 def notify(name, *args, **kwargs):
     """
     Send notifications. If ``backends==None``, all backends with the same name
-    will be used to deliver a notification. 
-    
+    will be used to deliver a notification.
+
     If ``backends`` is a list, only the specified backends will be used.
-    
+
     :param name: The notification to send
     :param backends: A list of backends to be used or ``None`` to use all associated backends
     """
     assert name in registry.notifications, "'{0}' is not a valid notification.".format(repr(name))
-    
+
     backends = kwargs.pop('backends', None)
-    
+
     if backends is None:
         backends = registry.notifications[name]
-    
+
     results = []
 
     for Backend in backends:
         backend = Backend()
         results.append(backend.notify(*args, **kwargs))
-    
+
     return results
-
-
--- yell/backends/django.py
+++ yell/backends/django.py
@@ -3,12 +3,14 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *
 import os
 from .django.conf import settings
 from .django import template
 from .django.core.mail import send_mail, EmailMultiAlternatives
 from yell import Notification
-
 
 
 class EmailBackend(Notification):
@@ -20,28 +22,27 @@ class EmailBackend(Notification):
     """ Email subject """
     body = None
     """ Email body """
-    
 
     def get_subject(self, *args, **kwargs):
         """
-        Return a subject. Override if you need templating or such. 
+        Return a subject. Override if you need templating or such.
         """
         return self.subject
-    
+
     def get_body(self, *args, **kwargs):
         """
         Return a body. Override if you need templating or such.
         """
         return self.body
-    
+
     def get_from(self, *args, **kwargs):
         return settings.DEFAULT_FROM_EMAIL
 
     def get_to(self, *args, **kwargs):
         return kwargs.get('to')
-    
+
     def notify(self, *args, **kwargs):
-        
+
         return send_mail(
             self.get_subject(*args, **kwargs),
             self.get_body(*args, **kwargs),
@@ -49,61 +50,62 @@ class EmailBackend(Notification):
             self.get_to(*args, **kwargs),
         )
 
+
 class MultipartEmailBackend(EmailBackend):
     """
     Send emails via :class:`django.core.mail.EmailMultiAlternatives`
     """
     default_content_type = 'text/plain'
     """
-    The default content type. 
+    The default content type.
     """
-    
+
     body = {}
     """
-    Email body mapping content type to message. Requires at least a key for 
+    Email body mapping content type to message. Requires at least a key for
     :attr:`default_content_type`.
     """
 
-    
     def get_body(self, *args, **kwargs):
         assert self.default_content_type in self.body, "No message body for default content type '%s'" % self.default_content_type
         return self.body
-    
+
     def get_default_body(self, *args, **kwargs):
         return self.get_body(*args, **kwargs)[self.default_content_type]
-    
-    def notify(self, *args, **kwargs):        
+
+    def notify(self, *args, **kwargs):
         message = EmailMultiAlternatives(
             self.get_subject(*args, **kwargs),
             self.get_default_body(*args, **kwargs),
             self.get_from(*args, **kwargs),
             self.get_to(*args, **kwargs)
         )
-        
-        for content_type, body in self.get_body(*args, **kwargs).items():
+
+        for content_type, body in list(self.get_body(*args, **kwargs).items()):
             if content_type == self.default_content_type: continue
             message.attach_alternative(body, content_type)
-        
+
         return message.send()
-        
+
+
 class TemplatedEmailBackend(MultipartEmailBackend):
     """
     Generates email bodies from templates.
-    
+
     :example:
-    
+
     ::
-    
+
         class SignupMessage(TemplatedEmailBackend):
             name = 'signup'
             subject = "Welcome to %s" % Site.objects.get_current().name
             content_types = ['text/plain', 'text/html']
-        
+
     The `SignupMessage` class would look up following templates for the email body:
-    
+
     * `yell/signup.txt`
     * `yell/signup.html`
-    
+
     """
     content_types = (
         ('text/plain', '.txt'),
@@ -112,10 +114,10 @@ class TemplatedEmailBackend(MultipartEmailBackend):
     """
     Default content types to render
     """
-        
+
     # Memoize
     _body = None
-    
+
     def get_path(self, name, ext):
         """
         Get the path to a given template name. Override if you wish to
@@ -124,7 +126,7 @@ class TemplatedEmailBackend(MultipartEmailBackend):
         return os.path.join('yell', '{0}{1}'.format(name, ext))
 
     def get_body(self, *args, **kwargs):
-        """ 
+        """
         Render message bodies by using all the content types defined
         in :attr:`content_types`
         """
@@ -134,4 +136,3 @@ class TemplatedEmailBackend(MultipartEmailBackend):
                 tpl = template.loader.get_template(self.get_path(self.name, ext))
                 self._body[ctype] = tpl.render(template.Context(kwargs))
         return self._body
-    
